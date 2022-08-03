08fdddb33ed15e0dbb98965ba68820e923a9f6fe
Vimal57 <vimals@zignuts.com>
2022-08-03 15:43:58 +0530

    create note, delete node donw without api integration

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,16 +1,18 @@
 import React, { useContext } from 'react';
 import noteContext from '../context/notes/NoteContext';
 import Noteitem from './Noteitem';
+import AddNote from './AddNote';
 
 function Notes() {
 	const context = useContext(noteContext);
-	const { notes, setNotes } = context;
+	const { notes } = context;
 	return (
 		<>
+            <AddNote />
 			<h1>Your Notes</h1>
 			<div className="row my-3">
 				{notes.map((note) => {
-					return <Noteitem note={note} />;
+					return <Noteitem key={note._id} note={note} />;
 				})}
 			</div>
 		</>
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -1,70 +1,93 @@
-import { useState } from "react";
-import NoteContext from "./NoteContext";
+import { useState } from 'react';
+import NoteContext from './NoteContext';
 
 const NoteState = (props) => {
+	const dummyNotes = [
+		{
+			_id: '62e90bf0ea21baa91d6c432d1',
+			user: '62de5e2bd6e3364ec99611e3',
+			title: 'birthday',
+			description: 'happy birthday',
+			tag: 'General',
+			date: '2022-08-02T11:35:12.471Z',
+			__v: 0,
+		},
+		{
+			_id: '62e90bffea21baa91d6c43322',
+			user: '62de5e2bd6e3364ec99611e3',
+			title: 'greetings',
+			description: 'good morning',
+			tag: 'General',
+			date: '2022-08-02T11:35:27.323Z',
+			__v: 0,
+		},
+		{
+			_id: '62e90bf0ea21baa91d6c432v3',
+			user: '62de5e2bd6e3364ec99611e3',
+			title: 'birthday',
+			description: 'happy birthday',
+			tag: 'General',
+			date: '2022-08-02T11:35:12.471Z',
+			__v: 0,
+		},
+		{
+			_id: '62e90bffea21baa91d6c433v4',
+			user: '62de5e2bd6e3364ec99611e3',
+			title: 'greetings',
+			description: 'good morning',
+			tag: 'General',
+			date: '2022-08-02T11:35:27.323Z',
+			__v: 0,
+		},
+		{
+			_id: '62e90bf0ea21baa91d6c432t5',
+			user: '62de5e2bd6e3364ec99611e3',
+			title: 'birthday',
+			description: 'happy birthday',
+			tag: 'General',
+			date: '2022-08-02T11:35:12.471Z',
+			__v: 0,
+		},
+		{
+			_id: '62e90bffea21baa91d6c433t6',
+			user: '62de5e2bd6e3364ec99611e3',
+			title: 'greetings',
+			description: 'good morning',
+			tag: 'General',
+			date: '2022-08-02T11:35:27.323Z',
+			__v: 0,
+		},
+	];
+	const [notes, setNotes] = useState(dummyNotes);
 
-    const dummyNotes = [
-        {
-          "_id": "62e90bf0ea21baa91d6c432d",
-          "user": "62de5e2bd6e3364ec99611e3",
-          "title": "birthday",
-          "description": "happy birthday",
-          "tag": "General",
-          "date": "2022-08-02T11:35:12.471Z",
-          "__v": 0
-        },
-        {
-          "_id": "62e90bffea21baa91d6c4332",
-          "user": "62de5e2bd6e3364ec99611e3",
-          "title": "greetings",
-          "description": "good morning",
-          "tag": "General",
-          "date": "2022-08-02T11:35:27.323Z",
-          "__v": 0
-        },
-        {
-          "_id": "62e90bf0ea21baa91d6c432d",
-          "user": "62de5e2bd6e3364ec99611e3",
-          "title": "birthday",
-          "description": "happy birthday",
-          "tag": "General",
-          "date": "2022-08-02T11:35:12.471Z",
-          "__v": 0
-        },
-        {
-          "_id": "62e90bffea21baa91d6c4332",
-          "user": "62de5e2bd6e3364ec99611e3",
-          "title": "greetings",
-          "description": "good morning",
-          "tag": "General",
-          "date": "2022-08-02T11:35:27.323Z",
-          "__v": 0
-        },
-        {
-          "_id": "62e90bf0ea21baa91d6c432d",
-          "user": "62de5e2bd6e3364ec99611e3",
-          "title": "birthday",
-          "description": "happy birthday",
-          "tag": "General",
-          "date": "2022-08-02T11:35:12.471Z",
-          "__v": 0
-        },
-        {
-          "_id": "62e90bffea21baa91d6c4332",
-          "user": "62de5e2bd6e3364ec99611e3",
-          "title": "greetings",
-          "description": "good morning",
-          "tag": "General",
-          "date": "2022-08-02T11:35:27.323Z",
-          "__v": 0
-        },
-      ]
-      const [notes, setNotes] = useState(dummyNotes)
-    return (
-        <NoteContext.Provider value={{notes, setNotes}}> 
-            {props.children}
-        </NoteContext.Provider>
-    )
-}
+	// Create Note
+	const createNote = (title, description, tag) => {
+		// TODO api call
+		console.log('create note');
+		setNotes(
+			notes.concat({
+				title,
+				description,
+				tag,
+			})
+		);
+	};
+
+	// Edit Note
+	const editNote = () => {};
+
+	// Delete Note
+	const deleteNote = (id) => {
+    // TODO api call
+    console.log("delete note :: ", id);
+    const newNotes = notes.filter(note => note._id !== id);
+    setNotes(newNotes);
+  };
+	return (
+		<NoteContext.Provider value={{ notes, createNote, editNote, deleteNote }}>
+			{props.children}
+		</NoteContext.Provider>
+	);
+};
 
 export default NoteState;
